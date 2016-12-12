8f7c12c3d28ad19cf846ccd1c84a380cb7c55158
Rebeca Gonzalez Revilla <rebeca.carlosiii@gmail.com>
2016-12-12 12:08:36 +0100

    funcion 2 y 3

--- finalproject.c
+++ finalproject.c
@@ -6,234 +6,234 @@
 
 int main(int argc, char const *argv[]) {
 //printf("Hello world %s!\n", argv[1] );
-  int semicolonPositionIterator=0;
-  int semicolonPosition[6]; //numero de ; en la estructura de pelicula
-  struct Movie mymovie;
-  FILE * fp;
-  char * line = NULL;
-  size_t len = 0;
-  ssize_t read;
-
-  //printf("my movie.title: %s\n",mymovie.title);
-
-  fp = fopen("movies.txt", "r");
-  if (fp == NULL)
-      exit(EXIT_FAILURE);
-
-  while ((read = getline(&line, &len, fp)) != -1) {
-      // printf("Retrieved line of length %zu :\n", read);
-      // printf("%s", line);
-      for (size_t i = 0; i <= read-1; i++) {
-        if (line[i]==';') {
-          semicolonPosition[semicolonPositionIterator]=i;
-          semicolonPositionIterator ++;
-        }
-      }
-      for (size_t i = 0; i < semicolonPosition[0]; i++) {
-        mymovie.title[i]=line[i];
-      }
-      printf("%s\n",mymovie.title);
-
-      for (int i = semicolonPosition[0]+1; i < semicolonPosition[1]; i++) {
-        mymovie.genre[i-(semicolonPosition[0]+1)]=line[i];
-      }
-      printf("%s\n", mymovie.genre);
-
-      for (int i = semicolonPosition[1]+1; i < semicolonPosition[2]; i++) {
-        mymovie.director[i-(semicolonPosition[1]+1)]=line[i];
-      }
-      printf("%s\n", mymovie.director);
-
-      char year[4];
-      for (int i = semicolonPosition[2]+1; i < semicolonPosition[3]; i++) {
-        year[i-(semicolonPosition[2]+1)]=line[i];
-      }
-      mymovie.year=atoi(year);
-      printf("%d\n", mymovie.year);
-
-      int colonPosition[MAX_NUM_ACTORS];
-      int colonPositionIterator=0;
-      for(int i = semicolonPosition[3]+1; i < semicolonPosition[4]; i++){
-        if (line[i]==':'){
-          colonPosition[colonPositionIterator]=i;
-          colonPositionIterator ++;
-        }
-      }
-
-      int numActors=colonPositionIterator+1;
-      for(int i=0; i<numActors;i++){
-        char actorInformation[MAX_CHAR_LENGTH]={'\0'};
-        if(i==0){
-          for (int j = semicolonPosition[3]+1; j < colonPosition[0]; j++) {
-            actorInformation[j-(semicolonPosition[3]+1)]=line[j];
-          }
-        }
-
-        else if(i==(numActors-1)){
-          for (int k = colonPosition[colonPositionIterator-1]+1; k < semicolonPosition[4]; k++) {
-            actorInformation[k-(colonPosition[colonPositionIterator-1]+1)]=line[k];
-          }
-        }
-        else{
-          for (int h = colonPosition[i-1]+1; h < colonPosition[i]; h++) {
-            actorInformation[h-(colonPosition[i-1]+1)]=line[h];
-          }
-        }
-
-        int plusPosition[2];
-        int plusPositionIterator=0;
-        for (int m = 0; m < strlen(actorInformation); m++) {
-          if (actorInformation[m]=='+') {
-            plusPosition[plusPositionIterator]=m;
-            plusPositionIterator ++;
-          }
-        }
-
-        for (int l = 0; l < plusPosition[0]; l++) {
-          mymovie.list_of_actors[i].name[l]=actorInformation[l];
-        }
-        // printf("nombre del actor %s\n",mymovie.list_of_actors[i].name);
-
-        char birthdateString[MAX_CHAR_LENGTH];
-        for (int l = plusPosition[0]+1; l < plusPosition[1]; l++) {
-          birthdateString[l-(plusPosition[0]+1)]=actorInformation[l];
-        }
-
-        int slashPosition[2];
-        int slashPositionIterator=0;
-        for (int m = 0; m < strlen(birthdateString); m++) {//aqui hay m pq quiero
-          if (birthdateString[m]=='/') {
-            slashPosition[slashPositionIterator]=m;
-            slashPositionIterator ++;
-          }
-        }
-
-        //convertir el dia (string-structure)
-        char day[2]={'\0'};
-        for (int m = 0; m < slashPosition[0]; m++) {
-          day[m]=birthdateString[m];
-        }
-        mymovie.list_of_actors[i].birthdateActor.day=atoi(day);
-
-        //convertir el mes (string-structure)
-        char month[2]={'\0'};
-        for (int m = slashPosition[0]+1; m < slashPosition[1]; m++) {
-          month[m-(slashPosition[0]+1)]=birthdateString[m];
-        }
-        mymovie.list_of_actors[i].birthdateActor.month=atoi(month);
-
-        //convertir el año (string-structure)
-        char year[4]={'\0'};
-        for (int m = slashPosition[1]+1; m < strlen(birthdateString); m++) {
-          year[m-(slashPosition[1]+1)]=birthdateString[m];
-        }
-        mymovie.list_of_actors[i].birthdateActor.year=atoi(year);
-
-        for (int l = plusPosition[1]+1; l < strlen(actorInformation); l++) {
-          mymovie.list_of_actors[i].nationality[l-(plusPosition[1]+1)]=actorInformation[l];
-        }
-        // printf("nacionalidad del actor %s\n",mymovie.list_of_actors[i].nationality);
-        printf("Actor%d: %s %d/%d/%d %s\n",i, mymovie.list_of_actors[i].name,
-                                              mymovie.list_of_actors[i].birthdateActor.day,
-                                              mymovie.list_of_actors[i].birthdateActor.month,
-                                              mymovie.list_of_actors[i].birthdateActor.year,
-                                              mymovie.list_of_actors[i].nationality);
-      }
-
-      // mymovie.description[semicolonPosition[5]]='\0';
-      for (int i = semicolonPosition[4]+1; i < semicolonPosition[5]; i++) {
-        mymovie.description[i-(semicolonPosition[4]+1)]=line[i];
-      }
-      printf("%s\n", mymovie.description);
-
-      char price[6];
-      for (int i = semicolonPosition[5]+1; i <= read-1; i++) {
-        price[i-(semicolonPosition[5]+1)]=line[i];
-      }
-      mymovie.price=atof(price);
-      printf("%.2f €\n", mymovie.price);
-
-      // struct Movie list_of_movies[NUM_MOVIES];
-      // for (int i = 1; i <= NUM_MOVIES; i++) {
-      //   list_of_movies[i]=mymovie;
-      // }
-
-  }
-  // Lectura de clientes
-  struct Client myclient;
-
-  fp = fopen("clients.txt", "r");
-  if (fp == NULL)
-      exit(EXIT_FAILURE);
-
-  while ((read = getline(&line, &len, fp)) != -1) {
-      // printf("Retrieved line of length %zu :\n", read);
-      // printf("%s", line);
-      int plusPosition[2];
-      int plusPositionIterator=0;
-       for (int m = 0; m <read; m++) {
-         if (line[m]=='+') {
-           plusPosition[plusPositionIterator]=m;
-           plusPositionIterator ++;
-         }
-       }
-
-       char clientName[MAX_CHAR_LENGTH]={'\0'};
-       for (int l = 0; l < plusPosition[0]; l++) {
-         clientName[l]=line[l];
-       }
-       strcpy(myclient.name,clientName);
-
-       char birthdateString[MAX_CHAR_LENGTH];
-       for (int l = plusPosition[0]+1; l < plusPosition[1]; l++) {
-         birthdateString[l-(plusPosition[0]+1)]=line[l];
-       }
-
-       int slashPosition[2];
-       int slashPositionIterator=0;
-       for (int m = 0; m < strlen(birthdateString); m++) {//aqui hay m pq quiero
-         if (birthdateString[m]=='/') {
-           slashPosition[slashPositionIterator]=m;
-           slashPositionIterator ++;
-         }
-       }
-
-       //convertir el dia (string-structure)
-       char day[2]={'\0'};
-       for (int m = 0; m < slashPosition[0]; m++) {
-         day[m]=birthdateString[m];
-       }
-       myclient.birthdateClient.day=atoi(day);
-
-       //convertir el mes (string-structure)
-       char month[2]={'\0'};
-       for (int m = slashPosition[0]+1; m < slashPosition[1]; m++) {
-         month[m-(slashPosition[0]+1)]=birthdateString[m];
-       }
-       myclient.birthdateClient.month=atoi(month);
-
-       //convertir el año (string-structure)
-       char year[4]={'\0'};
-       for (int m = slashPosition[1]+1; m < strlen(birthdateString); m++) {
-         year[m-(slashPosition[1]+1)]=birthdateString[m];
-       }
-       myclient.birthdateClient.year=atoi(year);
-
-       char clientId [MAX_CHAR_LENGTH]={'\0'};
-       for (int l = plusPosition[1]+1; l < read; l++) {
-         clientId[l-(plusPosition[1]+1)]=line[l];
-       }
-       strcpy(myclient.id,clientId);
-       printf("Client: %s %d/%d/%d %s\n", myclient.name,
-                                          myclient.birthdateClient.day,
-                                          myclient.birthdateClient.month,
-                                          myclient.birthdateClient.year,
-                                          myclient.id);
-    }
-
-  fclose(fp);
-  if (line)
-      free(line);
+  // int semicolonPositionIterator=0;
+  // int semicolonPosition[6]; //numero de ; en la estructura de pelicula
+  // struct Movie mymovie;
+  // FILE * fp;
+  // char * line = NULL;
+  // size_t len = 0;
+  // ssize_t read;
+  // struct Movie list_of_movies[NUM_MOVIES];
+  // int listOfMoviesIterator=0;
+  //
+  // //printf("my movie.title: %s\n",mymovie.title);
+  //
+  // fp = fopen("movies.txt", "r");
+  // if (fp == NULL)
+  //     exit(EXIT_FAILURE);
+  //
+  // while ((read = getline(&line, &len, fp)) != -1) {
+  //     // printf("Retrieved line of length %zu :\n", read);
+  //     // printf("%s", line);
+  //     for (size_t i = 0; i <= read-1; i++) {
+  //       if (line[i]==';') {
+  //         semicolonPosition[semicolonPositionIterator]=i;
+  //         semicolonPositionIterator ++;
+  //       }
+  //     }
+  //     for (size_t i = 0; i < semicolonPosition[0]; i++) {
+  //       mymovie.title[i]=line[i];
+  //     }
+  //     printf("%s\n",mymovie.title);
+  //
+  //     for (int i = semicolonPosition[0]+1; i < semicolonPosition[1]; i++) {
+  //       mymovie.genre[i-(semicolonPosition[0]+1)]=line[i];
+  //     }
+  //     printf("%s\n", mymovie.genre);
+  //
+  //     for (int i = semicolonPosition[1]+1; i < semicolonPosition[2]; i++) {
+  //       mymovie.director[i-(semicolonPosition[1]+1)]=line[i];
+  //     }
+  //     printf("%s\n", mymovie.director);
+  //
+  //     char year[4];
+  //     for (int i = semicolonPosition[2]+1; i < semicolonPosition[3]; i++) {
+  //       year[i-(semicolonPosition[2]+1)]=line[i];
+  //     }
+  //     mymovie.year=atoi(year);
+  //     printf("%d\n", mymovie.year);
+  //
+  //     int colonPosition[MAX_NUM_ACTORS];
+  //     int colonPositionIterator=0;
+  //     for(int i = semicolonPosition[3]+1; i < semicolonPosition[4]; i++){
+  //       if (line[i]==':'){
+  //         colonPosition[colonPositionIterator]=i;
+  //         colonPositionIterator ++;
+  //       }
+  //     }
+  //
+  //     int numActors=colonPositionIterator+1;
+  //     for(int i=0; i<numActors;i++){
+  //       char actorInformation[MAX_CHAR_LENGTH]={'\0'};
+  //       if(i==0){
+  //         for (int j = semicolonPosition[3]+1; j < colonPosition[0]; j++) {
+  //           actorInformation[j-(semicolonPosition[3]+1)]=line[j];
+  //         }
+  //       }
+  //
+  //       else if(i==(numActors-1)){
+  //         for (int k = colonPosition[colonPositionIterator-1]+1; k < semicolonPosition[4]; k++) {
+  //           actorInformation[k-(colonPosition[colonPositionIterator-1]+1)]=line[k];
+  //         }
+  //       }
+  //       else{
+  //         for (int h = colonPosition[i-1]+1; h < colonPosition[i]; h++) {
+  //           actorInformation[h-(colonPosition[i-1]+1)]=line[h];
+  //         }
+  //       }
+  //
+  //       int plusPosition[2];
+  //       int plusPositionIterator=0;
+  //       for (int m = 0; m < strlen(actorInformation); m++) {
+  //         if (actorInformation[m]=='+') {
+  //           plusPosition[plusPositionIterator]=m;
+  //           plusPositionIterator ++;
+  //         }
+  //       }
+  //
+  //       for (int l = 0; l < plusPosition[0]; l++) {
+  //         mymovie.list_of_actors[i].name[l]=actorInformation[l];
+  //       }
+  //       // printf("nombre del actor %s\n",mymovie.list_of_actors[i].name);
+  //
+  //       char birthdateString[MAX_CHAR_LENGTH];
+  //       for (int l = plusPosition[0]+1; l < plusPosition[1]; l++) {
+  //         birthdateString[l-(plusPosition[0]+1)]=actorInformation[l];
+  //       }
+  //
+  //       int slashPosition[2];
+  //       int slashPositionIterator=0;
+  //       for (int m = 0; m < strlen(birthdateString); m++) {//aqui hay m pq quiero
+  //         if (birthdateString[m]=='/') {
+  //           slashPosition[slashPositionIterator]=m;
+  //           slashPositionIterator ++;
+  //         }
+  //       }
+  //
+  //       //convertir el dia (string-structure)
+  //       char day[2]={'\0'};
+  //       for (int m = 0; m < slashPosition[0]; m++) {
+  //         day[m]=birthdateString[m];
+  //       }
+  //       mymovie.list_of_actors[i].birthdateActor.day=atoi(day);
+  //
+  //       //convertir el mes (string-structure)
+  //       char month[2]={'\0'};
+  //       for (int m = slashPosition[0]+1; m < slashPosition[1]; m++) {
+  //         month[m-(slashPosition[0]+1)]=birthdateString[m];
+  //       }
+  //       mymovie.list_of_actors[i].birthdateActor.month=atoi(month);
+  //
+  //       //convertir el año (string-structure)
+  //       char year[4]={'\0'};
+  //       for (int m = slashPosition[1]+1; m < strlen(birthdateString); m++) {
+  //         year[m-(slashPosition[1]+1)]=birthdateString[m];
+  //       }
+  //       mymovie.list_of_actors[i].birthdateActor.year=atoi(year);
+  //
+  //       for (int l = plusPosition[1]+1; l < strlen(actorInformation); l++) {
+  //         mymovie.list_of_actors[i].nationality[l-(plusPosition[1]+1)]=actorInformation[l];
+  //       }
+  //       // printf("nacionalidad del actor %s\n",mymovie.list_of_actors[i].nationality);
+  //       printf("Actor%d: %s %d/%d/%d %s\n",i, mymovie.list_of_actors[i].name,
+  //                                             mymovie.list_of_actors[i].birthdateActor.day,
+  //                                             mymovie.list_of_actors[i].birthdateActor.month,
+  //                                             mymovie.list_of_actors[i].birthdateActor.year,
+  //                                             mymovie.list_of_actors[i].nationality);
+  //     }
+  //
+  //     // mymovie.description[semicolonPosition[5]]='\0';
+  //     for (int i = semicolonPosition[4]+1; i < semicolonPosition[5]; i++) {
+  //       mymovie.description[i-(semicolonPosition[4]+1)]=line[i];
+  //     }
+  //     printf("%s\n", mymovie.description);
+  //
+  //     char price[6];
+  //     for (int i = semicolonPosition[5]+1; i <= read-1; i++) {
+  //       price[i-(semicolonPosition[5]+1)]=line[i];
+  //     }
+  //     mymovie.price=atof(price);
+  //     printf("%.2f €\n", mymovie.price);
+  //
+  //     list_of_movies[listOfMoviesIterator]=mymovie;
+  //       listOfMoviesIterator ++;
+  //
+  // }
+  // // Lectura de clientes
+  // struct Client myclient;
+  //
+  // fp = fopen("clients.txt", "r");
+  // if (fp == NULL)
+  //     exit(EXIT_FAILURE);
+  //
+  // while ((read = getline(&line, &len, fp)) != -1) {
+  //     // printf("Retrieved line of length %zu :\n", read);
+  //     // printf("%s", line);
+  //     int plusPosition[2];
+  //     int plusPositionIterator=0;
+  //      for (int m = 0; m <read; m++) {
+  //        if (line[m]=='+') {
+  //          plusPosition[plusPositionIterator]=m;
+  //          plusPositionIterator ++;
+  //        }
+  //      }
+  //
+  //      char clientName[MAX_CHAR_LENGTH]={'\0'};
+  //      for (int l = 0; l < plusPosition[0]; l++) {
+  //        clientName[l]=line[l];
+  //      }
+  //      strcpy(myclient.name,clientName);
+  //
+  //      char birthdateString[MAX_CHAR_LENGTH];
+  //      for (int l = plusPosition[0]+1; l < plusPosition[1]; l++) {
+  //        birthdateString[l-(plusPosition[0]+1)]=line[l];
+  //      }
+  //
+  //      int slashPosition[2];
+  //      int slashPositionIterator=0;
+  //      for (int m = 0; m < strlen(birthdateString); m++) {//aqui hay m pq quiero
+  //        if (birthdateString[m]=='/') {
+  //          slashPosition[slashPositionIterator]=m;
+  //          slashPositionIterator ++;
+  //        }
+  //      }
+  //
+  //      //convertir el dia (string-structure)
+  //      char day[2]={'\0'};
+  //      for (int m = 0; m < slashPosition[0]; m++) {
+  //        day[m]=birthdateString[m];
+  //      }
+  //      myclient.birthdateClient.day=atoi(day);
+  //
+  //      //convertir el mes (string-structure)
+  //      char month[2]={'\0'};
+  //      for (int m = slashPosition[0]+1; m < slashPosition[1]; m++) {
+  //        month[m-(slashPosition[0]+1)]=birthdateString[m];
+  //      }
+  //      myclient.birthdateClient.month=atoi(month);
+  //
+  //      //convertir el año (string-structure)
+  //      char year[4]={'\0'};
+  //      for (int m = slashPosition[1]+1; m < strlen(birthdateString); m++) {
+  //        year[m-(slashPosition[1]+1)]=birthdateString[m];
+  //      }
+  //      myclient.birthdateClient.year=atoi(year);
+  //
+  //      char clientId [MAX_CHAR_LENGTH]={'\0'};
+  //      for (int l = plusPosition[1]+1; l < read; l++) {
+  //        clientId[l-(plusPosition[1]+1)]=line[l];
+  //      }
+  //      strcpy(myclient.id,clientId);
+  //      printf("Client: %s %d/%d/%d %s\n", myclient.name,
+  //                                         myclient.birthdateClient.day,
+  //                                         myclient.birthdateClient.month,
+  //                                         myclient.birthdateClient.year,
+  //                                         myclient.id);
+  //   }
+  //
+  // fclose(fp);
+  // if (line)
+  //     free(line);
 
   MainMenu();
 
--- functionslibrary.c
+++ functionslibrary.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "functionslibrary.h"
 
 void WatchOnline(){
@@ -27,13 +28,202 @@ void WatchOnline(){
     // scanf(" %c",&anyKey);
     getc(stdin);// ToDo como hacer que vuelva presionando cualquier tecla
   }
+  // FILE *fp;
+  // char * line = NULL;
+  // size_t len = 0;
+  // ssize_t read;
+  //
+  // fp = fopen("clients.txt", "a");
+  // if (fp == NULL){
+  //     exit(EXIT_FAILURE);
+  // }else{
+  //   fprintf(fp "%s %d %s\n",userName,cardNumber,expirationDate);
+  // }
+  // fclose(fp);
+  // if (line)
+  //     free(line);
 }
 void ShowOnline(){
-
+  char t1[]="Rogue One";
+  	char t2[]="Iron Man";
+  	char t3[]="Singin' in the rain";
+  	char t4[]="Fantastic beasts and where to find them";
+  	char t5[]="Doctor Strange";
+  	char t6[]="Arrival";
+  	char t7[]="Master and Commander";
+  	char t8[]="Skyfall";
+  	char t9[]="The Revenant";
+  	char t10[]="Casino Royale";
+  	char option[15];
+    printf("Introduce the title of a movie: \n");
+  	scanf(" %99[^\n]", option);
+  	if(strcmp(t1, option)==0){
+  		FILE* Rogueone= fopen ("movie1.txt", "r");
+  		char RO;
+  		if (Rogueone== NULL){
+  			printf("the file cannot be opened");
+  			};
+  		while (feof(Rogueone)==0){
+  			RO= getc(Rogueone);
+  			printf("%c", RO);
+  			}
+  		printf("Press any key to return to the main menu ......\n");
+      	getc(stdin);
+  	}else if(strcmp(t2, option)==0){
+  		FILE* IronMan= fopen ("movie2.txt", "r");
+  		char RO;
+  		if (IronMan== NULL){
+  			printf("the file cannot be opened");
+  			};
+  		while (feof(IronMan)==0){
+  			RO= getc(IronMan);
+  			printf("%c", RO);
+  			}
+  		printf("Press any key to return to the main menu ......\n");
+      	getc(stdin);
+  	}else if(strcmp(t3, option)==0){
+  		FILE* Singin= fopen ("movie3.txt", "r");
+  		char RO;
+  		if (Singin== NULL){
+  			printf("the file cannot be opened");
+  			};
+  		while (feof(Singin)==0){
+  			RO= getc(Singin);
+  			printf("%c", RO);
+  			}
+  		printf("Press any key to return to the main menu ......\n");
+      	getc(stdin);
+  	}else if(strcmp(t4, option)==0){
+  		FILE* Fanstastic= fopen ("movie4.txt", "r");
+  		char RO;
+  		if (Fanstastic== NULL){
+  			printf("the file cannot be opened");
+  			};
+  		while (feof(Fanstastic)==0){
+  			RO= getc(Fanstastic);
+  			printf("%c", RO);
+  			}
+  		printf("Press any key to return to the main menu ......\n");
+      	getc(stdin);
+  	}else if(strcmp(t5, option)==0){
+  			FILE* Doctor= fopen ("movie5.txt", "r");
+  		char RO;
+  		if (Doctor== NULL){
+  			printf("the file cannot be opened");
+  			};
+  		while (feof(Doctor)==0){
+  			RO= getc(Doctor);
+  			printf("%c", RO);
+  			}
+  		printf("Press any key to return to the main menu ......\n");
+      	getc(stdin);
+  	}else if(strcmp(t6, option)==0){
+  		FILE* Arrival= fopen ("movie6.txt", "r");
+  		char RO;
+  		if (Arrival== NULL){
+  			printf("the file cannot be opened");
+  			};
+  		while (feof(Arrival)==0){
+  			RO= getc(Arrival);
+  			printf("%c", RO);
+  			}
+  		printf("Press any key to return to the main menu ......\n");
+      	getc(stdin);
+  	}else if(strcmp(t7, option)==0){
+  		FILE* Master= fopen ("movie7.txt", "r");
+  		char RO;
+  		if (Master== NULL){
+  			printf("the file cannot be opened");
+  			};
+  		while (feof(Master)==0){
+  			RO= getc(Master);
+  			printf("%c", RO);
+  			}
+  		printf("Press any key to return to the main menu ......\n");
+      	getc(stdin);
+  	}else if(strcmp(t8, option)==0){
+  		FILE* Skyfall= fopen ("movie8.txt", "r");
+  		char RO;
+  		if (Skyfall== NULL){
+  			printf("the file cannot be opened");
+  			};
+  		while (feof(Skyfall)==0){
+  			RO= getc(Skyfall);
+  			printf("%c", RO);
+  			}
+  		printf("Press any key to return to the main menu ......\n");
+      	getc(stdin);
+  	}else if(strcmp(t9, option)==0){
+  		FILE* Revenant= fopen ("movie9.txt", "r");
+  		char RO;
+  		if (Revenant== NULL){
+  			printf("the file cannot be opened");
+  			};
+  		while (feof(Revenant)==0){
+  			RO= getc(Revenant);
+  			printf("%c", RO);
+  			}
+  		printf("Press any key to return to the main menu ......\n");
+      	getc(stdin);
+  	}else if(strcmp(t10, option)==0){
+  		FILE* Casino= fopen ("movie10.txt", "r");
+  		char RO;
+  		if (Casino== NULL){
+  			printf("the file cannot be opened");
+  			};
+  		while (feof(Casino)==0){
+  			RO= getc(Casino);
+  			printf("%c", RO);
+  			}
+  		printf("Press any key to return to the main menu ......\n");
+      	getc(stdin);
+  	}
 }
 void RentDvd(){
-
-}
+    char c;
+  	char title [MAX_CHAR_LENGTH];
+  	char name [MAX_CHAR_LENGTH];
+  	float price, total;
+  	int d1=0, d2=0, m1=0, y1=0, n, card, available=1, d3, m3, y3;
+  	printf ("Introduce the title of the movie\n");
+  	scanf (" %99[^\n]", title);
+  	do{
+  		do{
+  			printf("introduce start date (dd/mm/yy)\n");
+  			scanf("%i %i %i", &d1, &m1, &y1);
+  			fflush(stdin);
+  		}while ((d1<1 || d1>31) || (m1<1 || m1>12));
+  		do{
+  			printf ("Introduce the number of days\n");
+   			scanf("%i", &n);
+   			fflush(stdin);
+  		}while (n<=0);
+  	       printf("introduce the price per day");
+  	      scanf("%f", &price);
+  	       fflush(stdin);
+  	        total= n*price;
+  	         printf("the total price is %f\n", total);
+  	}while (available==0);
+  	fflush(stdin);
+  	printf("do you want to continue(y/n)?\n");
+  	scanf("%c", &c);
+  	fflush(stdin);
+  	printf("\n");
+  	if (c!='y' || c!= 'Y'){
+  		printf("Introduce complete name\n");
+  		scanf("%[^\n]", name);
+  		fflush(stdin);
+  		printf("introduce card number\n");
+  		scanf("%i", &card);
+  		fflush(stdin);
+  		printf("Introduce the expire date of the credit card(dd/mm/yy)\n");
+  		scanf("%i %i %i", &d3, &m3, &y3);
+  		d2= d1+n;
+  		printf ("Great! The payment was successful. Please, get the DVD movie. You have to return the DVD movie next %i %i %i\n", d2, m1, y1);
+  		}
+  		printf("Press any key to return to the main menu ......\n");
+      	getc(stdin);
+  }
 void ShowRentals(){
 
 }
